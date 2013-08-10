db8b01be986c8dd4b79ea8e790ba4ec93377c030
Yoann Le Montagner <yo35@melix.net>
2013-08-10 23:45:36 +0200

    Template for the admin page header

---
+++ templates/admin/header.php
@@ -0,0 +1,7 @@
+
+<h2>
+	<div id="rpbchessboard-admin-icon">
+		<img src="<?php echo RPBCHESSBOARD_URL."/images/admin.png"; ?>" />
+	</div>
+	<?php _e('Chess games and diagrams', 'rpbchessboard'); ?>
+</h2>
