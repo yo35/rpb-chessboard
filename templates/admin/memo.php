b7426d2587bb400f8da0ef27b4835ce29c3cbc8d
Yoann Le Montagner <yo35@melix.net>
2013-08-10 22:05:19 +0200

    Create memo template

---
+++ templates/admin/memo.php
@@ -0,0 +1,18 @@
+
+<h3><?php _e('Reminder', 'rpbchessboard'); ?></h3>
+
+<h4><?php _e('FEN diagram alone', 'rpbchessboard'); ?></h4>
+
+TODO
+
+
+
+<h4><?php _e('Simple PGN game', 'rpbchessboard'); ?></h4>
+
+TODO
+
+
+
+<h4><?php _e('PGN game with comments, variations and diagrams', 'rpbchessboard'); ?></h4>
+
+TODO
