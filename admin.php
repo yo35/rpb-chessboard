3e678482c65392a3364e1b514d4c0e4c939a5c7c
Yoann Le Montagner <yo35@melix.net>
2013-08-10 22:05:42 +0200

    Create memo template

--- admin.php
+++ admin.php
@@ -2,5 +2,11 @@
 
 function rpbchessboard_admin_menu()
 {
-	echo '<p>TODO</p>';
+	//echo '<p>TODO</p>';
+	//ob_start();
+	echo '<div class="wrap">';
+	echo '<h2>'.__('Chess games and diagrams', 'rpbchessboard').'</h2>';
+	include(RPBCHESSBOARD_ABSPATH.'templates/admin/memo.php');
+	echo '</div>';
+	//return ob_get_clean();
 }
