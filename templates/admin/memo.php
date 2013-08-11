6b2d5d92afbf1c868b342f4a215b0025c03e0d86
Yoann Le Montagner <yo35@melix.net>
2013-08-11 15:44:03 +0200

    Shorter game as example

--- templates/admin/memo.php
+++ templates/admin/memo.php
@@ -76,29 +76,18 @@
 		<div class="rpbchessboard-admin-code-block">
 			[pgn]
 			<br/><br/>
-			[Event "World Championship 28th"]<br/>
-			[Site "Reykjavik"]<br/>
-			[Date "1972.08.10"]<br/>
-			[Round "13"]<br/>
-			[White "Spassky, Boris V."]<br/>
-			[Black "Fischer, Robert James"]<br/>
-			[Result "0-1"]<br/>
-			[WhiteElo "2660"]<br/>
-			[BlackElo "2785"]<br/>
+			[Event "London, England"]<br/>
+			[Site "London, England"]<br/>
+			[Date "1912.10.29"]<br/>
+			[Round "?"]<br/>
+			[White "Lasker, Edward"]<br/>
+			[Black "Thomas, George Alan"]<br/>
+			[Result "1-0"]<br/>
 			<br/>
-			1. e4 Nf6 2. e5 Nd5 3. d4 d6 4. Nf3 g6 5. Bc4 Nb6 6. Bb3 Bg7 7. Nbd2 O-O
-			8. h3 a5 9. a4 dxe5 10. dxe5 Na6 11. O-O Nc5 12. Qe2 Qe8 13. Ne4 Nbxa4
-			14. Bxa4 Nxa4 15. Re1 Nb6 16. Bd2 a4 17. Bg5 h6 18. Bh4 Bf5 19. g4 Be6
-			20. Nd4 Bc4 21. Qd2 Qd7 22. Rad1 Rfe8 23. f4 Bd5 24. Nc5 Qc8 25. Qc3 e6
-			26. Kh2 Nd7 27. Nd3 c5 28. Nb5 Qc6 29. Nd6 Qxd6 30. exd6 Bxc3 31. bxc3 f6
-			32. g5 hxg5 33. fxg5 f5 34. Bg3 Kf7 35. Ne5+ Nxe5 36. Bxe5 b5 37. Rf1 Rh8
-			38. Bf6 a3 39. Rf4 a2 40. c4 Bxc4 41. d7 Bd5 42. Kg3 Ra3+ 43. c3 Rha8
-			44. Rh4 e5 45. Rh7+ Ke6 46. Re7+ Kd6 47. Rxe5 Rxc3+ 48. Kf2 Rc2+
-			49. Ke1 Kxd7 50. Rexd5+ Kc6 51. Rd6+ Kb7 52. Rd7+ Ka6 53. R7d2 Rxd2
-			54. Kxd2 b4 55. h4 Kb5 56. h5 c4 57. Ra1 gxh5 58. g6 h4 59. g7 h3
-			60. Be7 Rg8 61. Bf8 h2 62. Kc2 Kc6 63. Rd1 b3+ 64. Kc3 h1=Q 65. Rxh1 Kd5
-			66. Kb2 f4 67. Rd1+ Ke4 68. Rc1 Kd3 69. Rd1+ Ke2 70. Rc1 f3 71. Bc5 Rxg7
-			72. Rxc4 Rd7 73. Re4+ Kf1 74. Bd4 f2 0-1
+			1. d4 e6 2. Nf3 f5 3. Nc3 Nf6 4. Bg5 Be7 5. Bxf6 Bxf6 6. e4
+			fxe4 7. Nxe4 b6 8. Ne5 O-O 9. Bd3 Bb7 10. Qh5 Qe7 11. Qxh7+
+			Kxh7 12. Nxf6+ Kh6 13. Neg4+ Kg5 14. h4+ Kf4 15. g3+ Kf3
+			16. Be2+ Kg2 17. Rh2+ Kg1 18. Kd2# 1-0
 			<br/><br/>
 			[/pgn]
 		</div>
@@ -127,28 +116,18 @@
 	<div class="rpbchessboard-admin-column-right">
 		<div class="rpbchessboard-admin-visu-block">
 			<pre class="jsChessLib-pgn-source" id="rpbchessboard-admin-example2-in">
-				[Event "World Championship 28th"]
-				[Site "Reykjavik"]
-				[Date "1972.08.10"]
-				[Round "13"]
-				[White "Spassky, Boris V."]
-				[Black "Fischer, Robert James"]
-				[Result "0-1"]
-				[WhiteElo "2660"]
-				[BlackElo "2785"]
+				[Event "London, England"]
+				[Site "London, England"]
+				[Date "1912.10.29"]
+				[Round "?"]
+				[White "Lasker, Edward"]
+				[Black "Thomas, George Alan"]
+				[Result "1-0"]
 
-				1. e4 Nf6 2. e5 Nd5 3. d4 d6 4. Nf3 g6 5. Bc4 Nb6 6. Bb3 Bg7 7. Nbd2 O-O 8. h3
-				a5 9. a4 dxe5 10. dxe5 Na6 11. O-O Nc5 12. Qe2 Qe8 13. Ne4 Nbxa4 14. Bxa4 Nxa4
-				15. Re1 Nb6 16. Bd2 a4 17. Bg5 h6 18. Bh4 Bf5 19. g4 Be6 20. Nd4 Bc4 21. Qd2
-				Qd7 22. Rad1 Rfe8 23. f4 Bd5 24. Nc5 Qc8 25. Qc3 e6 26. Kh2 Nd7 27. Nd3 c5 28.
-				Nb5 Qc6 29. Nd6 Qxd6 30. exd6 Bxc3 31. bxc3 f6 32. g5 hxg5 33. fxg5 f5 34. Bg3
-				Kf7 35. Ne5+ Nxe5 36. Bxe5 b5 37. Rf1 Rh8 38. Bf6 a3 39. Rf4 a2 40. c4 Bxc4 41.
-				d7 Bd5 42. Kg3 Ra3+ 43. c3 Rha8 44. Rh4 e5 45. Rh7+ Ke6 46. Re7+ Kd6 47. Rxe5
-				Rxc3+ 48. Kf2 Rc2+ 49. Ke1 Kxd7 50. Rexd5+ Kc6 51. Rd6+ Kb7 52. Rd7+ Ka6 53.
-				R7d2 Rxd2 54. Kxd2 b4 55. h4 Kb5 56. h5 c4 57. Ra1 gxh5 58. g6 h4 59. g7 h3 60.
-				Be7 Rg8 61. Bf8 h2 62. Kc2 Kc6 63. Rd1 b3+ 64. Kc3 h1=Q 65. Rxh1 Kd5 66. Kb2 f4
-				67. Rd1+ Ke4 68. Rc1 Kd3 69. Rd1+ Ke2 70. Rc1 f3 71. Bc5 Rxg7 72. Rxc4 Rd7 73.
-				Re4+ Kf1 74. Bd4 f2 0-1
+				1. d4 e6 2. Nf3 f5 3. Nc3 Nf6 4. Bg5 Be7 5. Bxf6 Bxf6 6. e4
+				fxe4 7. Nxe4 b6 8. Ne5 O-O 9. Bd3 Bb7 10. Qh5 Qe7 11. Qxh7+
+				Kxh7 12. Nxf6+ Kh6 13. Neg4+ Kg5 14. h4+ Kf4 15. g3+ Kf3
+				16. Be2+ Kg2 17. Rh2+ Kg1 18. Kd2# 1-0
 			</pre>
 			<div class="jsChessLib-invisible" id="rpbchessboard-admin-example2-out">
 				<div class="rpbchessboard-game-head">
