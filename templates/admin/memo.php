d10e93ba7ba10d2b68ea5880a95b208ae5666f0f
Yoann Le Montagner <yo35@melix.net>
2013-08-11 18:44:53 +0200

    Modify the PGN example

--- templates/admin/memo.php
+++ templates/admin/memo.php
@@ -68,7 +68,7 @@
 
 
 
-<h4><?php _e('Simple PGN game', 'rpbchessboard'); ?></h4>
+<h4><?php _e('PGN game', 'rpbchessboard'); ?></h4>
 
 <div class="rpbchessboard-admin-columns">
 
@@ -76,20 +76,42 @@
 		<div class="rpbchessboard-admin-code-block">
 			[pgn]
 			<br/><br/>
-			[Event "London, England"]<br/>
-			[Site "London, England"]<br/>
-			[Date "1912.10.29"]<br/>
-			[Round "?"]<br/>
-			[White "Lasker, Edward"]<br/>
-			[Black "Thomas, George Alan"]<br/>
-			[Result "1-0"]<br/>
+			[Event "1&lt;sup&gt;st&lt;/sup&gt; American Chess Congress"]<br/>
+			[Site "New York, NY USA"]<br/>
+			[Date "1857.11.03"]<br/>
+			[Round "4.6"]<br/>
+			[White "Louis Paulsen"]<br/>
+			[Black "Paul Morphy"]<br/>
+			[Result "0-1"]<br/>
 			<br/>
-			1. d4 e6 2. Nf3 f5 3. Nc3 Nf6 4. Bg5 Be7 5. Bxf6 Bxf6 6. e4
-			fxe4 7. Nxe4 b6 8. Ne5 O-O 9. Bd3 Bb7 10. Qh5 Qe7 11. Qxh7+
-			Kxh7 12. Nxf6+ Kh6 13. Neg4+ Kg5 14. h4+ Kf4 15. g3+ Kf3
-			16. Be2+ Kg2 17. Rh2+ Kg1 18. Kd2# 1-0
+			1. e4 e5 2. Nf3 Nc6 3. Nc3 Nf6 4. Bb5 Bc5 5. O-O O-O 6. Nxe5 Re8
+			7. Nxc6 dxc6 8. Bc4 b5 9. Be2 Nxe4 10. Nxe4 Rxe4 11. Bf3 Re6
+			12. c3 Qd3 13. b4 Bb6 14. a4 bxa4 15. Qxa4 Bd7 16. Ra2 Rae8
+			17. Qa6<br/>
+			<br/>
+			{[pgndiagram]
+			<?php
+				_e(
+					'Morphy took twelve minutes over his next move, '.
+					'probably to assure himself that the combination was sound and '.
+					'that he had a forced win in every variation.',
+				'rpbchessboard');
+			?>}<br/>
+			<br/>
+			17... Qxf3 $3 18. gxf3 Rg6+ 19. Kh1 Bh3 20. Rd1<br/>
+			<br/>
+			({<?php _e('Not', 'rpbchessboard'); ?>} 20. Rg1 Rxg1+ 21. Kxg1 Re1+ $19)<br/>
+			<br/>
+			20... Bg2+ 21. Kg1 Bxf3+ 22. Kf1 Bg2+<br/>
+			<br/>
+			(22...Rg2 $1 {<?php _e('would have won more quickly. For instance:', 'rpbchessboard'); ?>}
+			23. Qd3 Rxf2+ 24. Kg1 Rg2+ 25. Kh1 Rg1#)<br/>
+			<br/>
+			23. Kg1 Bh3+ 24. Kh1 Bxf2 25. Qf1
+			{<?php _e('Absolutely forced.', 'rpbchessboard'); ?>}
+			25... Bxf1 26. Rxf1 Re2 27. Ra1 Rh6 28. d4 Be3 0-1
 			<br/><br/>
-			[/pgn]
+			[/png]
 		</div>
 		<p>
 			<?php echo sprintf(
@@ -115,161 +137,43 @@
 
 	<div class="rpbchessboard-admin-column-right">
 		<div class="rpbchessboard-admin-visu-block">
-			<pre class="jsChessLib-pgn-source" id="rpbchessboard-admin-example2-in">
-				[Event "London, England"]
-				[Site "London, England"]
-				[Date "1912.10.29"]
-				[Round "?"]
-				[White "Lasker, Edward"]
-				[Black "Thomas, George Alan"]
-				[Result "1-0"]
-
-				1. d4 e6 2. Nf3 f5 3. Nc3 Nf6 4. Bg5 Be7 5. Bxf6 Bxf6 6. e4
-				fxe4 7. Nxe4 b6 8. Ne5 O-O 9. Bd3 Bb7 10. Qh5 Qe7 11. Qxh7+
-				Kxh7 12. Nxf6+ Kh6 13. Neg4+ Kg5 14. h4+ Kf4 15. g3+ Kf3
-				16. Be2+ Kg2 17. Rh2+ Kg1 18. Kd2# 1-0
-			</pre>
-			<div class="jsChessLib-invisible" id="rpbchessboard-admin-example2-out">
-				<div class="rpbchessboard-game-head">
-					<div class="jsChessLib-field-fullNameWhite">
-						<span class="rpbchessboard-white-square">&nbsp;</span>
-						<span class="jsChessLib-anchor-fullNameWhite"></span>
-					</div>
-					<div class="jsChessLib-field-fullNameBlack">
-						<span class="rpbchessboard-black-square">&nbsp;</span>
-						<span class="jsChessLib-anchor-fullNameBlack"></span>
-					</div>
-					<div class="jsChessLib-field-Event">
-						<span class="jsChessLib-anchor-Event"></span>
-						<span class="jsChessLib-field-Round">(<span class="jsChessLib-anchor-Round"></span>)</span>
-						<span class="jsChessLib-field-Date">- <span class="jsChessLib-anchor-Date"></span></span>
-					</div>
-				</div>
-				<div class="rpbchessboard-game-body jsChessLib-field-moves">
-					<span class="jsChessLib-anchor-moves"></span>
-					<div class="jsChessLib-field-Result">
-						<span class="jsChessLib-anchor-Result"></span>
-					</div>
-				</div>
-			</div>
-			<script type="text/javascript">
-				jsChessRenderer.processPGNByID("rpbchessboard-admin-example2");
-			</script>
-		</div>
-	</div>
-
-</div>
-
-
-
-
-
-<h4><?php _e('PGN game with comments, variations and diagrams', 'rpbchessboard'); ?></h4>
-
-<div class="rpbchessboard-admin-columns">
-
-	<div class="rpbchessboard-admin-column-left">
-		<div class="rpbchessboard-admin-code-block">
-			[pgn]
-			<br/><br/>
-			[Event "<?php _e('Blitz tournament aside from the Cap d\'Agde Open', 'rpbchessboard'); ?>"]<br/>
-			[Site "Cap d'Agde FRA"]<br/>
-			[Date "2010.10.28"]<br/>
-			[Round "?"]<br/>
-			[White "<?php _e('Anonymous 1', 'rpbchessboard'); ?>"]<br/>
-			[Black "<?php _e('Anonymous 2', 'rpbchessboard'); ?>"]<br/>
-			[WhiteElo "1800"]<br/>
-			[BlackElo "2050"]<br/>
-			[Result "0-1"]<br/>
-			[Annotator "<?php _e('Anonymous 2, who apologizes beforehand for this game!', 'rpbchessboard'); ?>"]<br/>
-			<br/>
-			1. e4 c5 2. c3 d5 3.exd5 Qxd5 4. d4 e5 5. Nf3 e4 6. Ne5 Nd7<br/>
-			<br/>
-			{On l'aura compris, les deux derniers coups des noirs visent à harceler le cavalier blanc.}<br/>
-			<br/>
-			7. Bc4 $1<br/>
-			<br/>
-			{[pgndiagram] J'avoue avoir sous-estimé la force du coup 7.Fc4.
-			En effet, en m'obligeant à reculer la dame en d6,
-			les blancs entravent gravement les perspectives de développement du fou f8.
-			Mis en difficulté sur le plan stratégique,
-			je décide donc à ce moment de rechercher des compensations tactiques,
-			et de tendre un piège subtil à mon adversaire.}<br/>
-			<br/>
-			7... Qd6 8. Nxf7<br/>
-			<br/>
-			{Victoire, il est tombé dans mon piège : il a lâché la case g6 !
-			Ni une, ni deux, je m'en empare...}<br/>
-			<br/>
-			8... Qg6 9. Nxh8 Qxg2 10.Rf1 Nb6 $5 11.Bxg8 $2<br/>
-			<br/>
-			{[pgndiagram]Je continue à renforcer ma position.
-			Le coup 10...Cb6 m'a ainsi permis de me débarasser de deux de mes pièces non-protégées :
-			d'une part la tour a8, fortement exposée de part sa position centrale,
-			et d'autre part le cavalier g8,
-			dont l'activité rayonnante n'était pas indispensable à l'exécution fructueuse de mon plan.
-			Au contraire, les blancs ont maintenant trois pièces non-protégées, en a1, g8 et h8.}<br/>
-			<br/>
-			11... Bh3 12. Bf7+ Kd8 13. Nd2 cxd4 14. cxd4 Bb4 15. a3 $6
-			{<?php _e('Unforseen move...', 'rpbchessboard'); ?>}
-			15...Qxf1# 0-1
-			<br/><br/>
-			[/png]
-		</div>
-	</div>
-
-	<div class="rpbchessboard-admin-column-right">
-		<div class="rpbchessboard-admin-visu-block">
 			<pre class="jsChessLib-pgn-source" id="rpbchessboard-admin-example3-in">
-				[Event "<?php _e('Blitz tournament aside from the Cap d\'Agde Open', 'rpbchessboard'); ?>"]
-				[Site "Cap d'Agde FRA"]
-				[Date "2010.10.28"]
-				[Round "?"]
-				[White "<?php _e('Anonymous 1', 'rpbchessboard'); ?>"]
-				[Black "<?php _e('Anonymous 2', 'rpbchessboard'); ?>"]
-				[WhiteElo "1800"]
-				[BlackElo "2050"]
+				[Event "1<sup>st</sup> American Chess Congress"]
+				[Site "New York, NY USA"]
+				[Date "1857.11.03"]
+				[Round "4.6"]
+				[White "Louis Paulsen"]
+				[Black "Paul Morphy"]
 				[Result "0-1"]
-				[Annotator "<?php _e('Anonymous 2, who apologizes beforehand for this game!', 'rpbchessboard'); ?>"]
 
-				1. e4 c5 2. c3 d5 3.exd5 Qxd5 4. d4 e5 5. Nf3 e4 6. Ne5 Nd7 $4 7. Bc4 $1
-
-				{<span class="jsChessLib-anchor-diagram"></span> J'avoue avoir sous-estimé la force du coup 7.Fc4.
-				En effet, en m'obligeant à reculer la dame en d6,
-				les blancs entravent gravement les perspectives de développement du fou f8.
-				Mis en difficulté sur le plan stratégique,
-				je décide donc à ce moment de rechercher des compensations tactiques,
-				et de tendre un piège subtil à mon adversaire.}
-
-				7... Qd6 8. Nxf7
-
-				{Victoire, il est tombé dans mon piège : il a lâché la case g6 !
-				Ni une, ni deux, je m'en empare...}
-
-				8... Qg6 9. Nxh8
-
-				({<?php _e('I must admit that on', 'rpbchessboard'); ?>}
-				9. O-O
-				{<?php _e('it would have been challenging for Black to come back in the game.', 'rpbchessboard'); ?>})
-
-				9... Qxg2 10.Rf1 Nb6 $5 11.Bxg8 $2
+				1. e4 e5 2. Nf3 Nc6 3. Nc3 Nf6 4. Bb5 Bc5 5. O-O O-O 6. Nxe5 Re8
+				7. Nxc6 dxc6 8. Bc4 b5 9. Be2 Nxe4 10. Nxe4 Rxe4 11. Bf3 Re6
+				12. c3 Qd3 13. b4 Bb6 14. a4 bxa4 15. Qxa4 Bd7 16. Ra2 Rae8
+				17. Qa6
 
 				{
 					<span class="jsChessLib-anchor-diagram"></span>
-					Je continue à renforcer ma position.
-					Le coup 10...Cb6 m'a ainsi permis de me débarasser de deux de mes pièces non-protégées :
-					d'une part la tour a8, fortement exposée de part sa position centrale,
-					et d'autre part le cavalier g8,
-					dont l'activité rayonnante n'était pas indispensable à l'exécution fructueuse de mon plan.
-					Au contraire, les blancs ont maintenant trois pièces non-protégées, en a1, g8 et h8.
+					<?php
+						_e(
+							'Morphy took twelve minutes over his next move, '.
+							'probably to assure himself that the combination was sound and '.
+							'that he had a forced win in every variation.',
+						'rpbchessboard');
+					?>
 				}
 
-				11... Bh3 12. Bf7+ Kd8 13. Nd2 cxd4 14. cxd4 Bb4 15. a3 $6
+				17... Qxf3 $3 18. gxf3 Rg6+ 19. Kh1 Bh3 20. Rd1
 
-				(15. Qe2 Ke7 16. a3 Bxd2+ 17. Bxd2 Qxf1+ 18. Qxf1 Bxf1 19. Kxf1 Rxh8 20. Bb3 $18
-				{<?php _e('and White has no problem.', 'rpbchessboard'); ?>})
+				({<?php _e('Not', 'rpbchessboard'); ?>} 20. Rg1 Rxg1+ 21. Kxg1 Re1+ $19)
 
-				15...Qxf1# 0-1
+				20... Bg2+ 21. Kg1 Bxf3+ 22. Kf1 Bg2+
+
+				(22...Rg2 $1 {<?php _e('would have won more quickly. For instance:', 'rpbchessboard'); ?>}
+				23. Qd3 Rxf2+ 24. Kg1 Rg2+ 25. Kh1 Rg1#)
+
+				23. Kg1 Bh3+ 24. Kh1 Bxf2 25. Qf1
+				{<?php _e('Absolutely forced.', 'rpbchessboard'); ?>}
+				25... Bxf1 26. Rxf1 Re2 27. Ra1 Rh6 28. d4 Be3 0-1
 			</pre>
 			<div class="jsChessLib-invisible" id="rpbchessboard-admin-example3-out">
 				<div class="rpbchessboard-game-head">
@@ -286,9 +190,6 @@
 						<span class="jsChessLib-field-Round">(<span class="jsChessLib-anchor-Round"></span>)</span>
 						<span class="jsChessLib-field-Date">- <span class="jsChessLib-anchor-Date"></span></span>
 					</div>
-					<div class="jsChessLib-field-Annotator"><?php
-						echo sprintf(__('Commented by %1$s', 'rpbchessboard'), '<span class="jsChessLib-anchor-Annotator"></span>');
-					?></div>
 				</div>
 				<div class="rpbchessboard-game-body jsChessLib-field-moves">
 					<span class="jsChessLib-anchor-moves"></span>
