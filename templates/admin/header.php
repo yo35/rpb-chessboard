acba9bbc2ce08a8f4e06d97c47b2e86fb8320e40
Yoann Le Montagner <yo35@melix.net>
2013-10-06 17:44:45 +0200

    Print a title given by model in the header

--- templates/admin/header.php
+++ templates/admin/header.php
@@ -3,5 +3,8 @@
 	<div id="rpbchessboard-admin-icon">
 		<img src="<?php echo RPBCHESSBOARD_URL."/images/admin.png"; ?>" />
 	</div>
-	<?php _e('Chess games and diagrams', 'rpbchessboard'); ?>
+	<?php
+		_e('Chess games and diagrams', 'rpbchessboard');
+		echo ' - ' . $model->getTitle();
+	?>
 </h2>
