457983a03d612677ef99acb79399f5feae31352b
Yoann Le Montagner <yo35@melix.net>
2013-10-06 17:48:24 +0200

    Adapt the backend-menu build method for MVC

--- backend.php
+++ backend.php
@@ -4,19 +4,40 @@
 add_action('admin_menu', 'rpbchessboard_admin_register_interface');
 function rpbchessboard_admin_register_interface()
 {
-	add_options_page(
+	// Create the menu
+	add_menu_page(
 		__('Chess games and diagrams', 'rpbchessboard'),
-		__('Chess games and diagrams', 'rpbchessboard'),
-		'manage_options', 'rpbchessboard-admin', 'rpbchessboard_admin_build_interface'
+		__('Chess', 'rpbchessboard'),
+		'manage_options', 'rpbchessboard', rpbchessboard_admin_page_memo
+	);
+
+	// Page "memo" (same slug code that for the menu, to make it the default page).
+	add_submenu_page('rpbchessboard',
+		__('Chess games and diagrams', 'rpbchessboard') . ' - ' . __('Memo', 'rpbchessboard'),
+		__('Memo', 'rpbchessboard'),
+		'manage_options', 'rpbchessboard', rpbchessboard_admin_page_memo
+	);
+
+	// Page "options"
+	add_submenu_page('rpbchessboard',
+		__('Chess games and diagrams', 'rpbchessboard') . ' - ' . __('Options', 'rpbchessboard'),
+		__('Options', 'rpbchessboard'),
+		'manage_options', 'rpbchessboard-options', rpbchessboard_admin_page_options
 	);
 }
 
 
-// Build the administration page
-function rpbchessboard_admin_build_interface()
+// Page hooks
+function rpbchessboard_admin_page_memo   () { rpbchessboard_load_controller('Memo'   ); }
+function rpbchessboard_admin_page_options() { rpbchessboard_load_controller('Options'); }
+
+
+// Load the controller with the required name, and execute it.
+function rpbchessboard_load_controller($shortname)
 {
-	echo '<div class="wrap">';
-	include(RPBCHESSBOARD_ABSPATH.'templates/admin/header.php');
-	include(RPBCHESSBOARD_ABSPATH.'templates/admin/memo.php');
-	echo '</div>';
+	$filename = strtolower($shortname);
+	$name     = 'RPBChessboardController' . $shortname;
+	require_once(RPBCHESSBOARD_ABSPATH.'controllers/admin/'.$filename.'.php');
+	$controller = new $name();
+	$controller->run();
 }
