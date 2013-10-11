643cea41b1d6d6e5fbdd21d46d709e0b55fc69f4
Yoann Le Montagner <yo35@melix.net>
2013-10-11 23:35:19 +0200

    Comment

--- models/traits/abstractactiontrait.php
+++ models/traits/abstractactiontrait.php
@@ -2,6 +2,7 @@
 
 require_once(RPBCHESSBOARD_ABSPATH.'models/traits/abstracttrait.php');
 
+
 /**
  * Base class for traits implementing the operations that should be executed to
  * answer to a HTML POST request.
