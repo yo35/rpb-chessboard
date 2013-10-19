8c154767ee1b9d85b9855cb8b9ab625f8018ef3f
Yoann Le Montagner <yo35@melix.net>
2013-10-19 21:41:38 +0200

    Implement the PGN template

--- templates/shortcode/pgn.php
+++ templates/shortcode/pgn.php
@@ -3,4 +3,37 @@
 	echo htmlspecialchars($model->getContent());
 ?></div>
 
-<div id="<?php echo htmlspecialchars($model->getTopLevelItemID()); ?>-out" class="rpbchessboard-out rpbchessboard-invisible"></div>
+<div id="<?php echo htmlspecialchars($model->getTopLevelItemID()); ?>-out" class="rpbchessboard-out rpbchessboard-invisible">
+	<div class="rpbchessboard-game-head">
+		<div class="PgnWidget-field-fullNameWhite">
+			<span class="rpbchessboard-white-square"></span>
+			<span class="PgnWidget-anchor-fullNameWhite"></span>
+		</div>
+		<div class="PgnWidget-field-fullNameBlack">
+			<span class="rpbchessboard-black-square"></span>
+			<span class="PgnWidget-anchor-fullNameBlack"></span>
+		</div>
+		<div class="PgnWidget-field-Event">
+			<span class="PgnWidget-anchor-Event"></span>
+			<span class="PgnWidget-field-Round">(<span class="PgnWidget-anchor-Round"></span>)</span>
+			<span class="PgnWidget-field-Date">- <span class="PgnWidget-anchor-Date"></span></span>
+		</div>
+		<div class="PgnWidget-field-Annotator">
+			<?php echo sprintf(__('Commented by %1$s', 'rpbchessboard'), '<span class="PgnWidget-anchor-Annotator"></span>'); ?>
+		</div>
+	</div>
+	<div class="rpbchessboard-game-body PgnWidget-field-moves">
+		<div class="PgnWidget-anchor-moves"></div>
+		<div class="PgnWidget-field-Result">
+			<span class="PgnWidget-anchor-Result"></span>
+		</div>
+	</div>
+</div>
+
+<script type="text/javascript">
+	processPGN(
+		<?php echo json_encode($model->getTopLevelItemID()); ?> + '-in',
+		<?php echo json_encode($model->getTopLevelItemID()); ?> + '-out',
+		{<?php echo $model->getCustomOptionsAsJavascript(); ?>}
+	);
+</script>
