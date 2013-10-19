c05466a4fa5a07275cade21fa77c5141be3de8b0
Yoann Le Montagner <yo35@melix.net>
2013-10-19 20:56:54 +0200

    Debug

--- templates/shortcode/fen.php
+++ templates/shortcode/fen.php
@@ -1,7 +1,7 @@
 
-<div id="<?php echo htmlspecialchars($model->getTopLevelItemID()); ?>-in" class="rpbchessboard-in">
-	<?php echo htmlspecialchars($model->getContent()); ?>
-</div>
+<div id="<?php echo htmlspecialchars($model->getTopLevelItemID()); ?>-in" class="rpbchessboard-in"><?php
+	echo htmlspecialchars($model->getContent());
+?></div>
 
 <div id="<?php echo htmlspecialchars($model->getTopLevelItemID()); ?>-out" class="rpbchessboard-out rpbchessboard-invisible"></div>
 
