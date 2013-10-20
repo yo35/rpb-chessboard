2a68cc5b592d046d44a0ffbe335e23287f851dc1
Yoann Le Montagner <yo35@melix.net>
2013-10-20 19:07:08 +0200

    Javascript warning in the admin

--- templates/admin/header.php
+++ templates/admin/header.php
@@ -8,3 +8,14 @@
 			htmlspecialchars($model->getTitle());
 	?>
 </h2>
+
+<div id="rpbchessboard-admin-javascript-warning" class="rpbchessboard-javascript-warning">
+	<?php
+		_e('To work properly, the RPB Chessboard plugin needs javascript to be activated in your browser.',
+			'rpbchessboard');
+	?>
+</div>
+
+<script type="text/javascript">
+	hideJavascriptWarning('rpbchessboard-admin-javascript-warning');
+</script>
