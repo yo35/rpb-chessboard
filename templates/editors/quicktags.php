16cfd3202ab67f3fdef1d5ba9666c2869a1e0e62
Yoann Le Montagner <yo35@melix.net>
2014-03-14 22:06:11 +0100

    Edition of the castle rights

--- templates/editors/quicktags.php
+++ templates/editors/quicktags.php
@@ -20,7 +20,7 @@
  ******************************************************************************/
 ?>
 
-<div id="rpbchessboard-editFen-dialog" class="rbpchessboard-invisible">
+<div id="rpbchessboard-editFen-dialog" class="rpbchessboard-invisible">
 	<div class="rpbchessboard-admin-columns">
 		<div style="width: 65%;">
 			<div id="rpbchessboard-editFen-chessboard"></div>
@@ -47,6 +47,26 @@
 					</label>
 					<input id="rpbchessboard-editFen-turn-b" type="radio" name="turn" value="b" />
 				</span>
+			</p>
+			<p>
+				<div><?php _e('Castling availability:', 'rpbchessboard'); ?></div>
+				<table id="rpbchessboard-editFen-castleRights"><tbody>
+					<tr>
+						<td></td>
+						<td>O-O-O</td>
+						<td>O-O</td>
+					</tr>
+					<tr>
+						<td><span class="rpbchessboard-editFen-turnFlag uichess-chessboard-sprite36" style="background-position: -216px -0px;"></span></td>
+						<td><input id="rpbchessboard-editFen-castle-bq" type="checkbox" /></td>
+						<td><input id="rpbchessboard-editFen-castle-bk" type="checkbox" /></td>
+					</tr>
+					<tr>
+						<td><span class="rpbchessboard-editFen-turnFlag uichess-chessboard-sprite36" style="background-position: -216px -36px;"></span></td>
+						<td><input id="rpbchessboard-editFen-castle-wq" type="checkbox" /></td>
+						<td><input id="rpbchessboard-editFen-castle-wk" type="checkbox" /></td>
+					</tr>
+				</tbody></table>
 			</p>
 			TODO
 		</div>
@@ -76,13 +96,15 @@
 	function rpbchessboard_editFenDialog($)
 	{
 		// Nothing to do if the dialog has already been initialized.
-		if(!$('#rpbchessboard-editFen-dialog').hasClass('rbpchessboard-invisible')) {
+		if(!$('#rpbchessboard-editFen-dialog').hasClass('rpbchessboard-invisible')) {
 			return;
 		}
+
 
 		// FEN widget.
 		var fen = $('#rpbchessboard-editFen-fen');
 		fen.prop('readonly', true);
+
 
 		// Create the chessboard widget.
 		var cb = $('#rpbchessboard-editFen-chessboard');
@@ -95,6 +117,7 @@
 		});
 		fen.val(cb.chessboard('option', 'position'));
 
+
 		// Turn widget.
 		$('#rpbchessboard-editFen-turn-' + cb.chessboard('turn')).prop('checked', true);
 		$('#rpbchessboard-editFen-turn input').each(function(index, e)
@@ -102,20 +125,45 @@
 			$(e).click(function() { cb.chessboard('turn', $(e).val()); });
 		});
 
+
+		// Castle rights
+		function resetCastleRights()
+		{
+			var castleRights = '';
+			if($('#rpbchessboard-editFen-castle-wk').prop('checked')) castleRights += 'K';
+			if($('#rpbchessboard-editFen-castle-wq').prop('checked')) castleRights += 'Q';
+			if($('#rpbchessboard-editFen-castle-bk').prop('checked')) castleRights += 'k';
+			if($('#rpbchessboard-editFen-castle-bq').prop('checked')) castleRights += 'q';
+			cb.chessboard('castleRights', castleRights);
+		}
+		var castleRights = cb.chessboard('castleRights');
+		$('#rpbchessboard-editFen-castle-wk').prop('checked', castleRights.contains('K')).click(resetCastleRights);
+		$('#rpbchessboard-editFen-castle-wq').prop('checked', castleRights.contains('Q')).click(resetCastleRights);
+		$('#rpbchessboard-editFen-castle-bk').prop('checked', castleRights.contains('k')).click(resetCastleRights);
+		$('#rpbchessboard-editFen-castle-bq').prop('checked', castleRights.contains('q')).click(resetCastleRights);
+
+
 		// Buttons 'reset' and 'clear'.
 		function resetPosition(fen)
 		{
 			cb.chessboard('option', 'position', fen);
 			$('#rpbchessboard-editFen-turn-' + cb.chessboard('turn')).prop('checked', true);
+			var castleRights = cb.chessboard('castleRights');
+			$('#rpbchessboard-editFen-castle-wk').prop('checked', castleRights.contains('K'));
+			$('#rpbchessboard-editFen-castle-wq').prop('checked', castleRights.contains('Q'));
+			$('#rpbchessboard-editFen-castle-bk').prop('checked', castleRights.contains('k'));
+			$('#rpbchessboard-editFen-castle-bq').prop('checked', castleRights.contains('q'));
 		}
 		$('#rpbchessboard-editFen-reset').button().click(function() { resetPosition('start'); });
 		$('#rpbchessboard-editFen-clear').button().click(function() { resetPosition('empty'); });
+
 
 		// Button 'cancel'.
 		$('#rpbchessboard-editFen-cancel a').click(function()
 		{
 			$('#rpbchessboard-editFen-dialog').dialog('close');
 		});
+
 
 		// Submit button.
 		$('#rpbchessboard-editFen-form').submit(function(event)
@@ -127,8 +175,9 @@
 			);
 		});
 
+
 		// Create the dialog.
-		$('#rpbchessboard-editFen-dialog').removeClass('rbpchessboard-invisible').dialog({
+		$('#rpbchessboard-editFen-dialog').removeClass('rpbchessboard-invisible').dialog({
 			autoOpen   : false,
 			modal      : true,
 			dialogClass: 'wp-dialog',
