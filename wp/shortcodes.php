1846edd72d2c4187a5b045282c4f3ca83aee6de4
Yoann Le Montagner <yo35@melix.net>
2014-05-14 12:09:15 +0200

    Avoid that the content of [pgn][/pgn] and [fen][/fen] is processed with wptexturize, wpautop, etc..

--- wp/shortcodes.php
+++ wp/shortcodes.php
@@ -41,31 +41,46 @@ abstract class RPBChessboardShortcodes
 		$compatibility = RPBChessboardHelperLoader::loadTrait('Compatibility');
 		$fenShortcode = $compatibility->getFENShortcode();
 		$pgnShortcode = $compatibility->getPGNShortcode();
+		self::$lowLevelShortcodes = array($fenShortcode, $pgnShortcode);
 
 		// Register the shortcodes
 		add_shortcode($fenShortcode, array(__CLASS__, 'callbackShortcodeFEN'       ));
 		add_shortcode($pgnShortcode, array(__CLASS__, 'callbackShortcodePGN'       ));
 		add_shortcode('pgndiagram' , array(__CLASS__, 'callbackShortcodePGNDiagram'));
+
+		// A high-priority filter is required to prevent the WP engine to perform some nasty operations
+		// (e.g. wptexturize, wpautop, etc...) on the text enclosed by the shortcodes.
+		//
+		// The priority level 8 what is used by the WP engine to process the special [embed] shortcode.
+		// As the same type of low-level operation is performed here, using this priority level seems to be a good choice.
+		// However, having "official" guidelines or core methods to achieve this would be desirable.
+		//
+		add_filter('the_content', array(__CLASS__, 'preprocessLowLevelShortcodes'), 8);
 	}
 
 
-	public static function callbackShortcodeFEN       ($atts, $content='') { return self::runShortcode('FEN'       , $atts, $content); }
-	public static function callbackShortcodePGN       ($atts, $content='') { return self::runShortcode('PGN'       , $atts, $content); }
-	public static function callbackShortcodePGNDiagram($atts, $content='') { return self::runShortcode('PGNDiagram', $atts, $content); }
+	public static function callbackShortcodeFEN       ($atts, $content) { return self::runShortcode('FEN'       , true , $atts, $content); }
+	public static function callbackShortcodePGN       ($atts, $content) { return self::runShortcode('PGN'       , true , $atts, $content); }
+	public static function callbackShortcodePGNDiagram($atts, $content) { return self::runShortcode('PGNDiagram', false, $atts, $content); }
 
 
 	/**
 	 * Process a shortcode.
 	 *
 	 * @param string $shortcodeName
+	 * @param boolean $lowLevel
 	 * @param array $atts
 	 * @param string $content
 	 * @return string
 	 */
-	private static function runShortcode($shortcodeName, $atts, $content)
+	private static function runShortcode($shortcodeName, $lowLevel, $atts, $content)
 	{
-		// TODO: reimplement this method with a dedicated controller
+		// The content of low-level shortcodes is supposed to have been saved in `self::$lowLevelShortcodeContent`.
+		if($lowLevel && isset($content) && isset(self::$lowLevelShortcodeContent[$content])) {
+			$content = self::$lowLevelShortcodeContent[$content];
+		}
 
+		// TODO: reimplement this method with a dedicated controller
 		// TODO: rename the shortcode models
 		$modelName = $shortcodeName;
 		switch($shortcodeName) {
@@ -84,4 +99,51 @@ abstract class RPBChessboardShortcodes
 		$view->display();
 		return ob_get_clean();
 	}
+
+
+	/**
+	 * Replace the content of the low-level shortcodes with their respective MD5 digest,
+	 * saving the original content in the associative array `self::$lowLevelShortcodeContent`.
+	 *
+	 * @param string $text
+	 * @return $text
+	 */
+	public static function preprocessLowLevelShortcodes($text)
+	{
+		$tagMask = implode('|', self::$lowLevelShortcodes);
+		$pattern = '/\\[(\\[?)(' . $tagMask . ')\\b([^\\]]*)\\](.*?)\\[\\/\\2\\](\\]?)/s';
+		return preg_replace_callback($pattern, array(__CLASS__, 'preprocessLowLevelShortcode'), $text);
+	}
+
+
+	/**
+	 * Replacement function for the low-level shortcodes.
+	 *
+	 * @param array $m Regular expression match array.
+	 * @return string
+	 */
+	private static function preprocessLowLevelShortcode($m)
+	{
+		// Allow the [[foo]...[/foo]] syntax for escaping a tag.
+		if($m[1]=='[' && $m[5]==']') {
+			return $m[0];
+		}
+
+		// General case: save the shortcode content, and replace it with its MD5 digest.
+		$digest = md5($m[4]);
+		self::$lowLevelShortcodeContent[$digest] = $m[4];
+		return '[' . $m[2] . $m[3] . ']' . $digest . '[/' . $m[2] . ']';
+	}
+
+
+	/**
+	 * Shortcodes that need their content to be processed in a low-level manner.
+	 */
+	private static $lowLevelShortcodes;
+
+
+	/**
+	 * Saved content of the low-level shortcodes, indexed with their respective MD5 digest.
+	 */
+	private static $lowLevelShortcodeContent = array();
 }
