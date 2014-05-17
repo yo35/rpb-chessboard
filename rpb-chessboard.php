786edcc42fbfb7be8d0533de6bd3bea3cdb4c021
Yoann Le Montagner <yo35@melix.net>
2014-05-17 09:14:20 +0200

    Remove backend.php

--- rpb-chessboard.php
+++ rpb-chessboard.php
@@ -110,7 +110,13 @@ function rpbchessboard_init_shortcodes()
 }
 
 
-// Load either the back-end or the front-end depending on the context
+// Custom buttons in the text editors.
 if(is_admin()) {
-	require_once(RPBCHESSBOARD_ABSPATH.'backend.php'); //TODO: remove
+	add_action('admin_print_footer_scripts', 'rpbchessboard_admin_customize_editors');
+	function rpbchessboard_admin_customize_editors()
+	{
+		require_once(RPBCHESSBOARD_ABSPATH . 'controllers/editors.php');
+		$controller = new RPBChessboardControllerEditors();
+		$controller->run();
+	}
 }
