3e45af40d07963202251bbcd3f41a831f70375a0
Yoann Le Montagner <yo35@melix.net>
2014-05-31 11:59:41 +0200

    Update the memo page to use $.chessgame

--- templates/adminpage/memo.php
+++ templates/adminpage/memo.php
@@ -78,14 +78,17 @@
 		<div class="rpbchessboard-admin-column-right">
 			<div class="rpbchessboard-admin-visu-block">
 				<p><?php _e('White to move and mate in two:', 'rpbchessboard'); ?></p>
-				<div id="rpbchessboard-admin-example1-in" class="rpbchessboard-in">
-					r2qkbnr/ppp2ppp/2np4/4N3/2B1P3/2N5/PPPP1PPP/R1BbK2R w KQkq - 0 6
-				</div>
-				<div id="rpbchessboard-admin-example1-out" class="rpbchessboard-out rpbchessboard-invisible"></div>
-				<script type="text/javascript">
-					processFEN('rpbchessboard-admin-example1-in', 'rpbchessboard-admin-example1-out',
-						{squareSize: 28});
-				</script>
+				<p>
+					<div id="rpbchessboard-admin-example1"></div>
+					<script type="text/javascript">
+						jQuery(document).ready(function($) {
+							$('#rpbchessboard-admin-example1').chessboard({
+								squareSize: 28,
+								position: 'r2qkbnr/ppp2ppp/2np4/4N3/2B1P3/2N5/PPPP1PPP/R1BbK2R w KQkq - 0 6'
+							});
+						});
+					</script>
+				</p>
 				<p>
 					<?php _e(
 						'This position is known as the Légal Trap. '.
@@ -175,49 +178,51 @@
 
 		<div class="rpbchessboard-admin-column-right">
 			<div class="rpbchessboard-admin-visu-block">
-				<div id="rpbchessboard-admin-example2-in" class="rpbchessboard-in">
-					[Event &quot;1&lt;sup&gt;st&lt;/sup&gt; American Chess Congress&quot;]
-					[Site &quot;New York, NY USA&quot;]
-					[Date &quot;1857.11.03&quot;]
-					[Round &quot;4.6&quot;]
-					[White &quot;Paulsen, Louis&quot;]
-					[Black &quot;Morphy, Paul&quot;]
-					[Result &quot;0-1&quot;]
-
-					1. e4 e5 2. Nf3 Nc6 3. Nc3 Nf6 4. Bb5 Bc5 5. O-O O-O 6. Nxe5 Re8
-					7. Nxc6 dxc6 8. Bc4 b5 9. Be2 Nxe4 10. Nxe4 Rxe4 11. Bf3 Re6
-					12. c3 Qd3 13. b4 Bb6 14. a4 bxa4 15. Qxa4 Bd7 16. Ra2 Rae8
-					17. Qa6
-
-					{
-						&lt;span class=&quot;PgnWidget-anchor-diagram&quot;&gt;&lt;/span&gt;
-						<?php
-							_e(
-								'Morphy took twelve minutes over his next move, '.
-								'probably to assure himself that the combination was sound and '.
-								'that he had a forced win in every variation.',
-							'rpbchessboard');
-						?>
-					}
-
-					17... Qxf3 !! 18. gxf3 Rg6+ 19. Kh1 Bh3 20. Rd1
-					({<?php _e('Not', 'rpbchessboard'); ?>} 20. Rg1 Rxg1+ 21. Kxg1 Re1+ -+)
-					20... Bg2+ 21. Kg1 Bxf3+ 22. Kf1 Bg2+
-
-					(22...Rg2 ! {<?php _e('would have won more quickly. For instance:', 'rpbchessboard'); ?>}
-					23. Qd3 Rxf2+ 24. Kg1 Rg2+ 25. Kh1 Rg1#)
-
-					23. Kg1 Bh3+ 24. Kh1 Bxf2 25. Qf1
-					{<?php _e('Absolutely forced.', 'rpbchessboard'); ?>}
-					25... Bxf1 26. Rxf1 Re2 27. Ra1 Rh6 28. d4 Be3 0-1
-				</div>
-				<div id="rpbchessboard-admin-example2-out" class="rpbchessboard-out rpbchessboard-invisible">
-					<?php include(RPBCHESSBOARD_ABSPATH.'templates/common/pgncontent.php'); ?>
-				</div>
-				<script type="text/javascript">
-					processPGN('rpbchessboard-admin-example2-in', 'rpbchessboard-admin-example2-out',
-						{squareSize: 28});
-				</script>
+				<p>
+					<div id="rpbchessboard-admin-example2"></div>
+					<script type="text/javascript">
+						jQuery(document).ready(function($) {
+							$('#rpbchessboard-admin-example2').chessgame({
+								diagramOptions: { squareSize: 28 },
+								navigationBoard: 'frame',
+								pgn:
+									'[Event "1<sup>st</sup> American Chess Congress"]\n' +
+									'[Site "New York, NY USA"]\n' +
+									'[Date "1857.11.03"]\n' +
+									'[Round "4.6"]\n' +
+									'[White "Paulsen, Louis"]\n' +
+									'[Black "Morphy, Paul"]\n' +
+									'[Result "0-1"]\n' +
+									'1. e4 e5 2. Nf3 Nc6 3. Nc3 Nf6 4. Bb5 Bc5 5. O-O O-O 6. Nxe5 Re8\n' +
+									'7. Nxc6 dxc6 8. Bc4 b5 9. Be2 Nxe4 10. Nxe4 Rxe4 11. Bf3 Re6\n' +
+									'12. c3 Qd3 13. b4 Bb6 14. a4 bxa4 15. Qxa4 Bd7 16. Ra2 Rae8 17. Qa6\n' +
+									'\n' +
+									'{<span class="uichess-chessgame-diagramAnchor"></span>' +
+									<?php
+										echo json_encode(__(
+											'Morphy took twelve minutes over his next move, '.
+											'probably to assure himself that the combination was sound and '.
+											'that he had a forced win in every variation.',
+										'rpbchessboard'));
+									?> +
+									'}\n' +
+									'\n' +
+									'17... Qxf3 !! 18. gxf3 Rg6+ 19. Kh1 Bh3 20. Rd1\n' +
+									'({' + <?php echo json_encode(__('Not', 'rpbchessboard')); ?> + '}\n' +
+									'20. Rg1 Rxg1+ 21. Kxg1 Re1+ -+)\n' +
+									'20... Bg2+ 21. Kg1 Bxf3+ 22. Kf1 Bg2+\n' +
+									'\n' +
+									'(22...Rg2 ! {' +
+									<?php echo json_encode(__('would have won more quickly. For instance:', 'rpbchessboard')); ?> +
+									'} 23. Qd3 Rxf2+ 24. Kg1 Rg2+ 25. Kh1 Rg1#)\n' +
+									'\n' +
+									'23. Kg1 Bh3+ 24. Kh1 Bxf2 25. Qf1\n' +
+									'{' + <?php echo json_encode(__('Absolutely forced.', 'rpbchessboard')); ?> + '}\n' +
+									'25... Bxf1 26. Rxf1 Re2 27. Ra1 Rh6 28. d4 Be3 0-1'
+							});
+						});
+					</script>
+				</p>
 			</div>
 		</div>
 
