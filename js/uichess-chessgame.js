0b36cf6e44b9faa7657204e65588fe66197eaaef
Yoann Le Montagner <yo35@melix.net>
2015-01-03 11:41:25 +0100

    Update copyright notice

--- js/uichess-chessgame.js
+++ js/uichess-chessgame.js
@@ -1,7 +1,7 @@
 /******************************************************************************
  *                                                                            *
- *    This file is part of RPB Chessboard, a Wordpress plugin.                *
- *    Copyright (C) 2013-2014  Yoann Le Montagner <yo35 -at- melix.net>       *
+ *    This file is part of RPB Chessboard, a WordPress plugin.                *
+ *    Copyright (C) 2013-2015  Yoann Le Montagner <yo35 -at- melix.net>       *
  *                                                                            *
  *    This program is free software: you can redistribute it and/or modify    *
  *    it under the terms of the GNU General Public License as published by    *
--- templates/adminpage/helpfen.php
+++ templates/adminpage/helpfen.php
@@ -1,8 +1,8 @@
 <?php
 /******************************************************************************
  *                                                                            *
- *    This file is part of RPB Chessboard, a Wordpress plugin.                *
- *    Copyright (C) 2013-2014  Yoann Le Montagner <yo35 -at- melix.net>       *
+ *    This file is part of RPB Chessboard, a WordPress plugin.                *
+ *    Copyright (C) 2013-2015  Yoann Le Montagner <yo35 -at- melix.net>       *
  *                                                                            *
  *    This program is free software: you can redistribute it and/or modify    *
  *    it under the terms of the GNU General Public License as published by    *
