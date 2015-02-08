83064b6954d13d591e9327ad149762fe88cd48a7
Yoann Le Montagner <yo35@melix.net>
2015-02-08 12:01:31 +0100

    Start cleaning TU

---
+++ assets/debug-js/common-rpbchess.js
@@ -0,0 +1,144 @@
+/******************************************************************************
+ *                                                                            *
+ *    This file is part of RPB Chessboard, a WordPress plugin.                *
+ *    Copyright (C) 2013-2015  Yoann Le Montagner <yo35 -at- melix.net>       *
+ *                                                                            *
+ *    This program is free software: you can redistribute it and/or modify    *
+ *    it under the terms of the GNU General Public License as published by    *
+ *    the Free Software Foundation, either version 3 of the License, or       *
+ *    (at your option) any later version.                                     *
+ *                                                                            *
+ *    This program is distributed in the hope that it will be useful,         *
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ *    GNU General Public License for more details.                            *
+ *                                                                            *
+ *    You should have received a copy of the GNU General Public License       *
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
+ *                                                                            *
+ ******************************************************************************/
+
+
+/* jshint unused:false */
+/* jshint globalstrict:true */
+'use strict';
+
+/* global RPBChess */
+/* global registerException */
+
+
+// Exception registration
+registerException(RPBChess.exceptions.IllegalArgument, function(e) { return 'illegal argument in ' + e.fun; });
+registerException(RPBChess.exceptions.InvalidFEN     , function(e) { return 'bad FEN >>' + e.fen + '<< => ' + e.message; });
+registerException(RPBChess.exceptions.InvalidNotation, function(e) { return 'bad SAN >>' + e.notation + '<< => ' + e.message; });
+
+
+/**
+ * Generate a predicate to check that an exception is an illegal-argument exception issued by the right function.
+ *
+ * @param {string} fun
+ * @returns {function}
+ */
+function checkIllegalArgument(fun) {
+	return function(e) { return (e instanceof RPBChess.exceptions.IllegalArgument) && (e.fun === fun); };
+}
+
+
+/**
+ * Generate a predicate to check that an exception is an invalid-FEN exception with the right message.
+ *
+ * @param {string} code
+ * @param ...
+ * @returns {function}
+ */
+function checkInvalidFEN(code) {
+
+	// Build the error message
+	var message = '<not an error message>';
+	if(typeof code === 'undefined') {
+		message = null;
+	}
+	else if(code in RPBChess.i18n) {
+		message = RPBChess.i18n[code];
+		for(var i=1; i<arguments.length; ++i) {
+			var re = new RegExp('\\{' + i + '\\}');
+			message = message.replace(re, arguments[i]);
+		}
+	}
+
+	// Generate the predicate
+	return function(e) {
+		return (e instanceof RPBChess.exceptions.InvalidFEN) && (message === null || message === e.message);
+	};
+}
+
+
+/**
+ * Convert a colored-piece object returned by the {@link Position#board()} getter into a string.
+ *
+ * @param {object} cp
+ * @returns {string}
+ */
+function wrapCP(cp) {
+	return (typeof cp === 'object') && ('piece' in cp) && ('color' in cp) ? (cp.color + ':' + cp.piece) : cp;
+}
+
+
+/**
+ * Convert a move descriptor into a string.
+ *
+ * @param {RPBChess.MoveDescriptor} descriptor
+ * @returns {string}
+ */
+function wrapMove(descriptor) {
+	return descriptor.from() + descriptor.to() + (descriptor.type()===RPBChess.movetype.PROMOTION ? descriptor.promotion().toUpperCase() : '');
+}
+
+
+/**
+ * Concatenate the legal flag and the king squares into a string.
+ *
+ * @param {RPBChess.Position} position
+ * @returns {string}
+ */
+function legalInfo(position) {
+	return position.isLegal() + ':' + position.kingSquare('w') + ':' + position.kingSquare('b');
+}
+
+
+/**
+ * Concatenate the FEN string describing the position with its legal flag and king squares.
+ *
+ * @param {RPBChess.Position} position
+ * @returns {string}
+ */
+function fenInfo(position) {
+	return position.fen() + ' ' + position.isLegal() + ' ' + position.kingSquare('w') + ' ' + position.kingSquare('b');
+}
+
+
+/**
+ * Concatenate the legal and the null-move-legal flags into a string
+ *
+ * @param {RPBChess.Position} position
+ * @returns {string}
+ */
+function nullMoveInfo(position) {
+	return position.isLegal() + ':' + position.isNullMoveLegal();
+}
+
+
+/**
+ * Concatenate the check/checkmate/stalemate flags into a string.
+ *
+ * @param {RPBChess.Position} position
+ * @returns {string}
+ */
+function ccsInfo(position) {
+	if(position.isLegal()) {
+		return position.isCheck() + ':' + position.isCheckmate() + ':' + position.isStalemate() + ':' + position.hasMove();
+	}
+	else {
+		return '';
+	}
+}
---
+++ assets/debug-js/common.js
@@ -0,0 +1,202 @@
+/******************************************************************************
+ *                                                                            *
+ *    This file is part of RPB Chessboard, a WordPress plugin.                *
+ *    Copyright (C) 2013-2015  Yoann Le Montagner <yo35 -at- melix.net>       *
+ *                                                                            *
+ *    This program is free software: you can redistribute it and/or modify    *
+ *    it under the terms of the GNU General Public License as published by    *
+ *    the Free Software Foundation, either version 3 of the License, or       *
+ *    (at your option) any later version.                                     *
+ *                                                                            *
+ *    This program is distributed in the hope that it will be useful,         *
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ *    GNU General Public License for more details.                            *
+ *                                                                            *
+ *    You should have received a copy of the GNU General Public License       *
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
+ *                                                                            *
+ ******************************************************************************/
+
+
+/* jshint unused:false */
+/* jshint globalstrict:true */
+'use strict';
+
+
+/**
+ * Number of tests performed.
+ */
+var testCounter = 0;
+
+
+/**
+ * Number of tests that succeed.
+ */
+var successCounter = 0;
+
+
+/**
+ * Hold the registered exceptions.
+ */
+var registeredExceptions = [];
+
+
+/**
+ * Register a new type of exception.
+ *
+ * @param {function} constructor
+ * @param {function} formatter
+ */
+function registerException(constructor, formatter) {
+	registeredExceptions.push({ constructor:constructor, formatter:formatter });
+}
+
+
+/**
+ * Refresh the global counters.
+ */
+function refreshCounters() {
+
+	// Total number of tests
+	var div1 = document.getElementById('testCounter');
+	div1.innerHTML = '';
+	div1.appendChild(document.createTextNode(testCounter + ' tests performed.'));
+
+	// Number of successes/failures
+	var div2 = document.getElementById('successCounter');
+	div2.innerHTML = '';
+	if(testCounter===successCounter) {
+		div2.appendChild(document.createTextNode('All tests were successful.'));
+		div2.className = 'allSuccessful';
+	}
+	else {
+		div2.appendChild(document.createTextNode((testCounter-successCounter) + ' tests failed.'));
+		div2.className = 'someFailure';
+	}
+}
+
+
+/**
+ * Base report method.
+ *
+ * @param {boolean} success
+ * @param {string} message
+ */
+function printMessage(success, message) {
+	var div = document.createElement('div');
+	div.className = 'message ' + (success ? 'success' : 'failure');
+	div.appendChild(document.createTextNode(message));
+	document.getElementById('report').appendChild(div);
+}
+
+
+/**
+ * Report that a test succeeded.
+ *
+ * @param {string} label
+ */
+function printSuccess(label) {
+	printMessage(true, label);
+}
+
+
+/**
+ * Print an error message to report that a function does not return what was expected.
+ *
+ * @param {string} label
+ * @param {mixed} observedValue
+ * @param {mixed} expectedValue
+ */
+function printBadValue(label, observedValue, expectedValue) {
+	var message = label + ' => observed >>' + observedValue + '<<';
+	if(typeof expectedValue === 'undefined') {
+		message += ' while error expected';
+	}
+	else {
+		message += ' while expected >>' + expectedValue + '<<';
+	}
+	printMessage(false, message);
+}
+
+
+/**
+ * Print an error message to report that an exception was thrown during a test.
+ *
+ * @param {string} label
+ * @param {mixed} exception
+ */
+function printException(label, exception) {
+	var message = label + ' => ';
+	var isRegisteredException = false;
+
+	// Try to match the exception with the registered types
+	for(var i=0; i<registeredExceptions.length; ++i) {
+		var re = registeredExceptions[i];
+		if(exception instanceof re.constructor) {
+			message += re.formatter(exception);
+			isRegisteredException = true;
+			break;
+		}
+	}
+
+	// Fallback case if the exception do not match the registered types
+	if(!isRegisteredException) {
+		message += 'unknown exception => ' + exception;
+	}
+
+	// Reporting
+	printMessage(false, message);
+}
+
+
+/**
+ * Check the returned value of a function.
+ *
+ * @param {string} label
+ * @param {function} fun
+ * @param {mixed} expectedValue
+ */
+function test(label, fun, expectedValue) {
+	try {
+		++testCounter;
+		var observedValue = fun();
+		if(observedValue === expectedValue) {
+			++successCounter;
+			printSuccess(label);
+		}
+		else {
+			printBadValue(label, observedValue, expectedValue);
+		}
+	}
+	catch(exception) {
+		printException(label, exception);
+	}
+	refreshCounters();
+}
+
+
+/**
+ * Check the exception thrown by a function.
+ *
+ * @param {string} label
+ * @param {function} fun
+ * @param {function} checkException
+ */
+function testError(label, fun, checkException) {
+	try {
+		++testCounter;
+		var observedValue = fun();
+		printBadValue(label, observedValue);
+	}
+	catch(exception) {
+		if(checkException(exception)) {
+			++successCounter;
+			printSuccess(label);
+		}
+		else {
+			printException(label, exception);
+		}
+	}
+	refreshCounters();
+}
