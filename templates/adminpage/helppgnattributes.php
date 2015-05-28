5649cb2608a172102706b813cb5333a820d7cdc0
Yoann Le Montagner <yo35@melix.net>
2015-05-28 18:17:27 +0200

    Description of the piece_symbols

--- templates/adminpage/helppgnattributes.php
+++ templates/adminpage/helppgnattributes.php
@@ -26,7 +26,7 @@
 		<?php echo sprintf(
 			__(
 				'Several parameters may be passed to the %1$s[%3$s][/%3$s]%2$s tags '.
-				'in order to affect how the PGN game is displayed. '.
+				'in order to customize how the PGN games are displayed. '.
 				'These parameters are presented in this page.',
 			'rpbchessboard'),
 			'<span class="rpbchessboard-sourceCode">',
@@ -36,12 +36,91 @@
 	</p>
 
 	<ol class="rpbchessboard-outline">
+		<li><a href="#rpbchessboard-pgnAttributePieceSymbols"><?php _e('Piece symbols', 'rpbchessboard'); ?></a></li>
 		<li><a href="#rpbchessboard-pgnAttributeNavigationBoard"><?php _e('Navigation board', 'rpbchessboard'); ?></a></li>
 		<li><a href="#rpbchessboard-pgnAttributeFlip"><?php _e('Board flipping', 'rpbchessboard'); ?></a></li>
 		<li><a href="#rpbchessboard-pgnAttributeSquareSize"><?php _e('Square size', 'rpbchessboard'); ?></a></li>
 		<li><a href="#rpbchessboard-pgnAttributeShowCoordinates"><?php _e('Show coordinates', 'rpbchessboard'); ?></a></li>
-		<li><a href="#rpbchessboard-pgnAttributePieceSymbols"><?php _e('Piece symbols', 'rpbchessboard'); ?></a></li>
 	</ol>
+
+
+
+	<h3 id="rpbchessboard-pgnAttributePieceSymbols"><?php _e('Piece symbols', 'rpbchessboard'); ?></h3>
+
+	<div class="rpbchessboard-columns">
+		<div>
+			<p>
+				<?php echo sprintf(__('The %1$s attribute controls how chess pieces are denoted in the move list.', 'rpbchessboard'),
+					'<span class="rpbchessboard-sourceCode">piece_symbols</span>'); ?>
+			</p>
+			<table>
+				<tbody>
+					<tr>
+						<th><?php _e('Value', 'rpbchessboard'); ?></th>
+						<th><?php _e('Description', 'rpbchessboard'); ?></th>
+					</tr>
+					<tr>
+						<td><a href="#" class="rpbchessboard-sourceCode rpbchessboard-pgnAttributePieceSymbols-value">native</a></td>
+						<td><?php _e('First character of the piece name in English.', 'rpbchessboard'); ?></td>
+					</tr>
+					<?php if($model->isPieceSymbolLocalizationAvailable()): ?>
+						<tr>
+							<td><a href="#" class="rpbchessboard-sourceCode rpbchessboard-pgnAttributePieceSymbols-value">localized</a></td>
+							<td><?php _e('First character of the piece name in the blog language.', 'rpbchessboard'); ?></td>
+						</tr>
+					<?php endif; ?>
+					<tr>
+						<td><a href="#" class="rpbchessboard-sourceCode rpbchessboard-pgnAttributePieceSymbols-value">figurines</a></td>
+						<td><span class="uichess-chessgame-alphaFont">K Q R B N P</span></td>
+					</tr>
+					<tr>
+						<td><a href="#" class="rpbchessboard-sourceCode rpbchessboard-pgnAttributePieceSymbols-value">(KDTLSB)</a></td>
+						<td>
+							<?php echo sprintf(
+								__(
+									'Any sequence of 6 capital letters surrounded with parenthesis is allowed to set custom symbols. '.
+									'For instance, with %1$s(KDTLSB)%2$s, K will be used to denote a king, D for a queen, T for a rook, '.
+									'L for a bishop, S for a knight and B for a pawn.',
+								'rpbchessboard'),
+								'<span class="rpbchessboard-sourceCode">',
+								'</span>',
+								'rto', 'gg'
+							); ?>
+						</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+		<div>
+			<div class="rpbchessboard-sourceCode">
+				<?php echo sprintf(
+					'[%1$s <strong>piece_symbols=<span id="rpbchessboard-pgnAttributePieceSymbols-sourceCodeExample">native</span></strong>] ... [/%1$s]',
+					htmlspecialchars($model->getPGNShortcode())
+				); ?>
+			</div>
+			<div class="rpbchessboard-visuBlock">
+				<div>
+					<div id="rpbchessboard-pgnAttributePieceSymbols-anchor"></div>
+					<script type="text/javascript">
+						jQuery(document).ready(function($) {
+							$('#rpbchessboard-pgnAttributePieceSymbols-anchor').chessgame({
+								pgn:
+									'1. d4 Nf6 2. c4 e6 3. Nc3 Bb4 4. Qc2 d5 5. cxd5 Qxd5 6. Nf3 Qf5 7. Qxf5 exf5 8. a3 Be7 ' +
+									'9. Bg5 Be6 10. e3 c6 11. Bd3 Nbd7 12. O-O h6 13. Bh4 a5 14. Rac1 O-O 15. Ne2 g5 ' +
+									'16. Bg3 Ne4 17. Nc3 Nxc3 18. Rxc3 Nf6 19. Rcc1 Rfd8 20. Rfd1 Rac8 1/2-1/2'
+							});
+							$('.rpbchessboard-pgnAttributePieceSymbols-value').click(function(e) {
+								e.preventDefault();
+								var value = $(this).text();
+								$('#rpbchessboard-pgnAttributePieceSymbols-anchor').chessgame('option', 'pieceSymbols', value);
+								$('#rpbchessboard-pgnAttributePieceSymbols-sourceCodeExample').text(value);
+							});
+						});
+					</script>
+				</div>
+			</div>
+		</div>
+	</div>
 
 
 
@@ -50,13 +129,8 @@
 	<div id="rpbchessboard-pgnAttributeNavigationBoard-content" class="rpbchessboard-columns">
 		<div>
 			<p>
-				<?php echo sprintf(
-					__(
-						'The %1$s parameter controls the position of the navigation board. '.
-						'The following table presents the available values for this parameter.',
-					'rpbchessboard'),
-					'<span class="rpbchessboard-sourceCode">navigation_board</span>'
-				);?>
+				<?php echo sprintf(__('The %1$s parameter controls the position of the navigation board.', 'rpbchessboard'),
+					'<span class="rpbchessboard-sourceCode">navigation_board</span>'); ?>
 			</p>
 			<table>
 				<tbody>
@@ -143,12 +217,6 @@
 
 
 	<h3 id="rpbchessboard-pgnAttributeShowCoordinates"><?php _e('Show coordinates', 'rpbchessboard'); ?></h3>
-
-	<p>TODO</p>
-
-
-
-	<h3 id="rpbchessboard-pgnAttributePieceSymbols"><?php _e('Piece symbols', 'rpbchessboard'); ?></h3>
 
 	<p>TODO</p>
 
