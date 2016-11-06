1760a528c76713f84f8b6a74c448e981233739ca
Yoann Le Montagner <yo35@melix.net>
2016-11-06 20:27:37 +0100

    Change version to 4.6

--- rpb-chessboard.php
+++ rpb-chessboard.php
@@ -26,14 +26,14 @@ Description: This plugin allows you to typeset and display chess diagrams and PG
 Text Domain: rpbchessboard
 Author: Yoann Le Montagner
 License: GPLv3
-Version: 4.5
+Version: 4.6
 */
 
 
 // Directories
 define('RPBCHESSBOARD_ABSPATH', plugin_dir_path(__FILE__));
 define('RPBCHESSBOARD_URL'    , plugin_dir_url (__FILE__));
-define('RPBCHESSBOARD_VERSION', '4.5');
+define('RPBCHESSBOARD_VERSION', '4.6');
 
 
 // Enable localization
