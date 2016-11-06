da41bf2c55ae64491529686e611d5b0894a38483
Yoann Le Montagner <yo35@melix.net>
2016-11-06 19:44:29 +0100

    Update help for scrollLeft/scrollRight

--- templates/adminpage/help/pgnattributes/navigationboard.php
+++ templates/adminpage/help/pgnattributes/navigationboard.php
@@ -68,6 +68,18 @@
 					<td><?php if($model->getDefaultNavigationBoard()==='floatRight'): ?><div class="rpbchessboard-tickIcon"></div><?php endif; ?></td>
 					<td><?php _e('The navigation board is displayed on the right of the move list.', 'rpbchessboard'); ?></td>
 				</tr>
+				<tr>
+					<td><a href="#" class="rpbchessboard-sourceCode rpbchessboard-pgnAttributeNavigationBoard-value">scrollLeft</a></td>
+					<td><?php if($model->getDefaultNavigationBoard()==='scrollLeft'): ?><div class="rpbchessboard-tickIcon"></div><?php endif; ?></td>
+					<td><?php _e('The navigation board is displayed on the left of the move list. ' .
+						'The move list becomes scrollable if it is higher than the navigation board.', 'rpbchessboard'); ?></td>
+				</tr>
+				<tr>
+					<td><a href="#" class="rpbchessboard-sourceCode rpbchessboard-pgnAttributeNavigationBoard-value">scrollRight</a></td>
+					<td><?php if($model->getDefaultNavigationBoard()==='scrollRight'): ?><div class="rpbchessboard-tickIcon"></div><?php endif; ?></td>
+					<td><?php _e('The navigation board is displayed on the right of the move list. ' .
+						'The move list becomes scrollable if it is higher than the navigation board.', 'rpbchessboard'); ?></td>
+				</tr>
 			</tbody>
 		</table>
 
@@ -89,21 +101,29 @@
 						$('#rpbchessboard-pgnAttributeNavigationBoard-anchor').chessgame({
 							navigationBoardOptions: { squareSize: 28 },
 							pgn:
-								'[Event "Linares 16<sup>th</sup>"]\n' +
-								'[Site "Linares, ESP"]\n' +
-								'[Date "1999.02.25"]\n' +
-								'[Round "4"]\n' +
-								'[White "Kasparov, Garry"]\n' +
-								'[Black "Kramnik, Vladimir"]\n' +
-								'[Result "1/2-1/2"]\n' +
-								'[WhiteElo "2812"]\n' +
-								'[BlackElo "2751"]\n' +
-								'[WhiteTitle "WCH"]\n' +
-								'[BlackTitle "GM"]\n' +
-								'[Annotator "?"]\n' +
-								'1. d4 Nf6 2. c4 e6 3. Nc3 Bb4 4. Qc2 d5 5. cxd5 Qxd5 6. Nf3 Qf5 7. Qxf5 exf5 8. a3 Be7 ' +
-								'9. Bg5 Be6 10. e3 c6 11. Bd3 Nbd7 12. O-O h6 13. Bh4 a5 14. Rac1 O-O 15. Ne2 g5 ' +
-								'16. Bg3 Ne4 17. Nc3 Nxc3 18. Rxc3 Nf6 19. Rcc1 Rfd8 20. Rfd1 Rac8 1/2-1/2'
+								'[Event "World Championship"]\n' +
+								'[Site "Reykjavik, ISL"]\n' +
+								'[Date "1972.08.10"]\n' +
+								'[Round "13"]\n' +
+								'[Result "0-1"]\n' +
+								'[White "Spassky, Boris"]\n' +
+								'[Black "Fischer, Robert J."]\n' +
+								'\n' +
+								'1. e4 Nf6 2. e5 Nd5 3. d4 d6 4. Nf3 g6 5. Bc4 Nb6 6. Bb3 Bg7 ' +
+								'7. Nbd2 O-O 8. h3 a5 9. a4 dxe5 10. dxe5 Na6 11. O-O Nc5 ' +
+								'12. Qe2 Qe8 13. Ne4 Nbxa4 14. Bxa4 Nxa4 15. Re1 Nb6 16. Bd2 a4 ' +
+								'17. Bg5 h6 18. Bh4 Bf5 19. g4 Be6 20. Nd4 Bc4 21. Qd2 Qd7 ' +
+								'22. Rad1 Rfe8 23. f4 Bd5 24. Nc5 Qc8 25. Qc3 e6 26. Kh2 Nd7 ' +
+								'27. Nd3 c5 28. Nb5 Qc6 29. Nd6 Qxd6 30. exd6 Bxc3 31. bxc3 f6 ' +
+								'32. g5 hxg5 33. fxg5 f5 34. Bg3 Kf7 35. Ne5+ Nxe5 36. Bxe5 b5 ' +
+								'37. Rf1 Rh8 38. Bf6 a3 39. Rf4 a2 40. c4 Bxc4 41. d7 Bd5 ' +
+								'42. Kg3 Ra3+ 43. c3 Rha8 44. Rh4 e5 45. Rh7+ Ke6 46. Re7+ Kd6 ' +
+								'47. Rxe5 Rxc3+ 48. Kf2 Rc2+ 49. Ke1 Kxd7 50. Rexd5+ Kc6 ' +
+								'51. Rd6+ Kb7 52. Rd7+ Ka6 53. R7d2 Rxd2 54. Kxd2 b4 55. h4 Kb5 ' +
+								'56. h5 c4 57. Ra1 gxh5 58. g6 h4 59. g7 h3 60. Be7 Rg8 61. Bf8 ' +
+								'h2 62. Kc2 Kc6 63. Rd1 b3+ 64. Kc3 h1=Q 65. Rxh1 Kd5 66. Kb2 ' +
+								'f4 67. Rd1+ Ke4 68. Rc1 Kd3 69. Rd1+ Ke2 70. Rc1 f3 71. Bc5 ' +
+								'Rxg7 72. Rxc4 Rd7 73. Re4+ Kf1 74. Bd4 f2 0-1'
 						});
 						$('.rpbchessboard-pgnAttributeNavigationBoard-value').click(function(e) {
 							e.preventDefault();
