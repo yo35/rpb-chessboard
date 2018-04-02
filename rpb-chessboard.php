cdecc37d9c8e647824ad9654a61628f10ad210a9
Yoann Le Montagner <yo35@melix.net>
2018-04-02 19:17:26 +0200

    Change version to 5.2

--- rpb-chessboard.php
+++ rpb-chessboard.php
@@ -27,14 +27,14 @@ Text Domain: rpb-chessboard
 Domain Path: /languages
 Author: Yoann Le Montagner
 License: GPLv3
-Version: 5.1.6
+Version: 5.2
 */
 
 
 // Directories
 define( 'RPBCHESSBOARD_ABSPATH', plugin_dir_path( __FILE__ ) );
 define( 'RPBCHESSBOARD_URL', plugin_dir_url( __FILE__ ) );
-define( 'RPBCHESSBOARD_VERSION', '5.1.6' );
+define( 'RPBCHESSBOARD_VERSION', '5.2' );
 
 
 // Enable localization
