35f4052d63673fbdfdf67c9057ec387fdb4f407e
Yoann Le Montagner <yo35@melix.net>
2018-05-01 20:47:21 +0200

    Fix page jump issue on iOS (#129)

--- js/rpbchess-ui-chessgame.js
+++ js/rpbchess-ui-chessgame.js
@@ -630,7 +630,7 @@
 		var body  = buildBody(widget);
 
 		// A hidden field to catch the keyboard events
-		var focusField = '<div class="rpbui-chessgame-focusFieldContainer"><input class="rpbui-chessgame-focusField" type="text" readonly="true" /></div>';
+		var focusField = '<div class="rpbui-chessgame-focusFieldContainer"><a href="#" class="rpbui-chessgame-focusField"></a></div>';
 
 		// Navigation board
 		var prefix = '';
