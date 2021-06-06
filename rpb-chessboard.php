97a2ca0447c7a59ad04ce4deba6c1850a59d5230
Yoann Le Montagner <yo35@melix.net>
2021-06-06 15:56:47 +0200

    Bump version

--- rpb-chessboard.php
+++ rpb-chessboard.php
@@ -27,14 +27,14 @@ Text Domain: rpb-chessboard
 Domain Path: /languages
 Author: Yoann Le Montagner
 License: GPLv3
-Version: 6.0.1
+Version: 6.0.2
 */
 
 
 // Directories
 define( 'RPBCHESSBOARD_ABSPATH', plugin_dir_path( __FILE__ ) );
 define( 'RPBCHESSBOARD_URL', plugin_dir_url( __FILE__ ) );
-define( 'RPBCHESSBOARD_VERSION', '6.0.1' );
+define( 'RPBCHESSBOARD_VERSION', '6.0.2' );
 
 
 // Enable localization
