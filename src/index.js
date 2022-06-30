71491e94499b5ef79701db9bde7da3e410666c42
Yoann Le Montagner <yo35@melix.net>
2022-06-30 09:46:29 +0200

    Fix plugin JS initialization to limit the impact of async/deferred JS loading (#242)

--- src/index.js
+++ src/index.js
@@ -31,38 +31,44 @@ import NavigationToolbar from './NavigationToolbar';
 import { FENEditorIcon, registerFENBlock } from './FENEditor';
 import { PGNEditorIcon, registerPGNBlock } from './PGNEditor';
 
-// Internationalization
-kokopuReactI18n.INVALID_FEN_ERROR_TITLE = RPBChessboard.i18n.FEN_PARSING_ERROR_TITLE;
-kokopuReactI18n.ANNOTATED_BY = RPBChessboard.i18n.PGN_ANNOTATED_BY;
-kokopuReactI18n.LINE = RPBChessboard.i18n.PGN_LINE_REF;
-kokopuReactI18n.PIECE_SYMBOLS = {
-	K: RPBChessboard.i18n.PIECE_SYMBOLS.K,
-	Q: RPBChessboard.i18n.PIECE_SYMBOLS.Q,
-	R: RPBChessboard.i18n.PIECE_SYMBOLS.R,
-	B: RPBChessboard.i18n.PIECE_SYMBOLS.B,
-	N: RPBChessboard.i18n.PIECE_SYMBOLS.N,
-	P: RPBChessboard.i18n.PIECE_SYMBOLS.P,
-};
 
-// Square size bounds
-RPBChessboard.availableSquareSize = {
-	min: Chessboard.minSquareSize(),
-	max: Chessboard.maxSquareSize(),
-};
+function initializePlugin() {
 
-// Theming
-Object.assign(Chessboard.colorsets(), RPBChessboard.customColorsets);
-Object.assign(Chessboard.piecesets(), RPBChessboard.customPiecesets);
-RPBChessboard.colorsetData = Chessboard.colorsets();
-RPBChessboard.piecesetData = Chessboard.piecesets();
+	// Internationalization
+	kokopuReactI18n.INVALID_FEN_ERROR_TITLE = RPBChessboard.i18n.FEN_PARSING_ERROR_TITLE;
+	kokopuReactI18n.ANNOTATED_BY = RPBChessboard.i18n.PGN_ANNOTATED_BY;
+	kokopuReactI18n.LINE = RPBChessboard.i18n.PGN_LINE_REF;
+	kokopuReactI18n.PIECE_SYMBOLS = {
+		K: RPBChessboard.i18n.PIECE_SYMBOLS.K,
+		Q: RPBChessboard.i18n.PIECE_SYMBOLS.Q,
+		R: RPBChessboard.i18n.PIECE_SYMBOLS.R,
+		B: RPBChessboard.i18n.PIECE_SYMBOLS.B,
+		N: RPBChessboard.i18n.PIECE_SYMBOLS.N,
+		P: RPBChessboard.i18n.PIECE_SYMBOLS.P,
+	};
 
-// Special colorsets and piecesets for theming edition
-RPBChessboard.editColorset = Chessboard.colorsets()._edit_ = {};
-RPBChessboard.editPieceset = Chessboard.piecesets()._edit_ = {};
+	// Square size bounds
+	RPBChessboard.availableSquareSize = {
+		min: Chessboard.minSquareSize(),
+		max: Chessboard.maxSquareSize(),
+	};
 
-// Block registration
-registerFENBlock();
-registerPGNBlock();
+	// Theming
+	Object.assign(Chessboard.colorsets(), RPBChessboard.customColorsets);
+	Object.assign(Chessboard.piecesets(), RPBChessboard.customPiecesets);
+	RPBChessboard.colorsetData = Chessboard.colorsets();
+	RPBChessboard.piecesetData = Chessboard.piecesets();
+
+	// Special colorsets and piecesets for theming edition
+	RPBChessboard.editColorset = Chessboard.colorsets()._edit_ = {};
+	RPBChessboard.editPieceset = Chessboard.piecesets()._edit_ = {};
+
+	// Block registration
+	// WARNING: only at the end, as these operations may fail in case of improperly configured website
+	// (in particular in case of deferred/async JS loading).
+	registerFENBlock();
+	registerPGNBlock();
+}
 
 
 // Icon rendering
@@ -147,3 +153,13 @@ RPBChessboard.renderPGN = function(targetJQueryElement, widgetArgs) {
 	/>;
 	ReactDOM.render(widget, targetJQueryElement.get(0));
 };
+
+
+try {
+	initializePlugin();
+}
+catch (e) {
+	/* eslint-disable no-console */
+	console.error('Error while initializing RPB Chessboard: the plugin may not work properly.', e);
+	/* eslint-enable no-console */
+}
