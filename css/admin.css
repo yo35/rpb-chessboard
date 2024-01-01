04914ebd1f2bbad6ed9f46c7d8fa887909a819b1
Yoann Le Montagner <yo35@melix.net>
2024-01-01 10:34:13 +0100

    Upgrade copyright notice

--- css/admin.css
+++ css/admin.css
@@ -1,7 +1,7 @@
 /******************************************************************************
  *                                                                            *
  *    This file is part of RPB Chessboard, a WordPress plugin.                *
- *    Copyright (C) 2013-2023  Yoann Le Montagner <yo35 -at- melix.net>       *
+ *    Copyright (C) 2013-2024  Yoann Le Montagner <yo35 -at- melix.net>       *
  *                                                                            *
  *    This program is free software: you can redistribute it and/or modify    *
  *    it under the terms of the GNU General Public License as published by    *
