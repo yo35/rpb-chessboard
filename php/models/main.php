b75a803ead84806666abd66e3e2ccae45657b1ce
Yoann Le Montagner <yo35@melix.net>
2025-01-01 15:24:19 +0100

    Upgrade copyright notice

--- php/models/main.php
+++ php/models/main.php
@@ -2,7 +2,7 @@
 /******************************************************************************
  *                                                                            *
  *    This file is part of RPB Chessboard, a WordPress plugin.                *
- *    Copyright (C) 2013-2024  Yoann Le Montagner <yo35 -at- melix.net>       *
+ *    Copyright (C) 2013-2025  Yoann Le Montagner <yo35 -at- melix.net>       *
  *                                                                            *
  *    This program is free software: you can redistribute it and/or modify    *
  *    it under the terms of the GNU General Public License as published by    *
