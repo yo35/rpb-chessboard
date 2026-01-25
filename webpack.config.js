226a5ba28b3187f10f252a998788423dc580e7b7
Yoann Le Montagner <yo35@melix.net>
2026-01-25 10:25:03 +0100

    Fix webpack config for .ogg asset files

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+/******************************************************************************
+ *                                                                            *
+ *    This file is part of RPB Chessboard, a WordPress plugin.                *
+ *    Copyright (C) 2013-2026  Yoann Le Montagner <yo35 -at- melix.net>       *
+ *                                                                            *
+ *    This program is free software: you can redistribute it and/or modify    *
+ *    it under the terms of the GNU General Public License as published by    *
+ *    the Free Software Foundation, either version 3 of the License, or       *
+ *    (at your option) any later version.                                     *
+ *                                                                            *
+ *    This program is distributed in the hope that it will be useful,         *
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ *    GNU General Public License for more details.                            *
+ *                                                                            *
+ *    You should have received a copy of the GNU General Public License       *
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
+ *                                                                            *
+ ******************************************************************************/
+
+
+const defaultConfig = require('@wordpress/scripts/config/webpack.config');
+
+module.exports = {
+    ...defaultConfig,
+    module: {
+        ...defaultConfig.module,
+        rules: [
+            ...defaultConfig.module.rules,
+            {
+                test: /\.ogg$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'sounds/[name].[hash:8][ext]', // Similar naming pattern as the other assets in the default config.
+                },
+            },
+        ],
+    },
+};
